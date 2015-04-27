86897ef8eddd991b0d976fde736ae715768aa4f9
Alessandro Chelli <alessandrochelli@gmail.com>
2015-04-27 11:09:14 +0200

    angela client

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('command', function(msg){
-    io.emit('command', msg);
+io.on('connection', function(cmd){
+  socket.on('angela.command', function(cmd){
+    exec(cmd, puts);
   });
 });
 

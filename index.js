c1facd10821e1d0fdc95c60da4ce66f172e9c2dd
Alessandro Chelli <alessandrochelli@gmail.com>
2015-05-05 11:59:51 +0200

    change in socket

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var socket = require('socket.io-client')('http://localhost');
+var socket = require('socket.io-client')('http://apio04.cloudapp.net');
 
 var sys = require('sys')
 var exec = require('child_process').exec;
@@ -12,11 +12,20 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-socket.on('connect', function(){});
-socket.on('angela.command', function(cmd){
-	exec(cmd, puts);
+socket.on('connect', function(){
+	console.log("Connected")
+	socket.on('angela.client.command', function(cmd){
+	console.log("Il cloud mi ha mandato ",cmd)
+	exec(cmd.cmd, puts);
 });
-socket.on('disconnect', function(){});
+});
+socket.on('angela.client.command', function(cmd){
+	console.log("Il cloud mi ha mandato ",cmd)
+	exec(cmd.cmd, puts);
+});
+socket.on('disconnect', function(){
+	console.log("disconnect")
+});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
